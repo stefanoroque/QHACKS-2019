d395ecc665f9114c84cb901c61012f3bf9cb94cd
Alessia Morin <15afm5@caslab.queensu.ca>
2019-02-02 18:52:06 -0500

    alessia

---
+++ select_composer.js
@@ -0,0 +1,83 @@
+//document.getElementById('composerButton').onclick = selectComposer;
+function selectComposer ()
+{
+    var x = document.getElementById("composer-name");
+
+    if (x.value=="Bach")
+    {
+        window.open("https://www.youtube.com/watch?v=6JQm5aSjX6g")
+    }
+    else if (x.value=="Albeniz")
+    {
+        window.open("https://www.youtube.com/watch?v=1Yyxf_htYvY")
+    }
+    else if (x.value=="Balakriew") {
+        window.open("https://www.youtube.com/watch?v=Em2ffoAKnRU")
+    }
+    else if (x.value=="Beethoven") {
+        window.open("https://www.youtube.com/watch?v=W-fFHeTX70Q")
+    }
+    else if (x.value=="Borodin") {
+        window.open("https://www.youtube.com/watch?v=c9SSQOkMypU")
+    }
+    else if (x.value=="Brahams") {
+        window.open("https://www.youtube.com/watch?v=zKrxesI3ziE")
+    }
+    else if (x.value=="Burgmueller") {
+        window.open("https://www.youtube.com/watch?v=0NRE8fupShU")
+    }
+    else if (x.value=="Chopin") {
+        window.open("https://www.youtube.com/watch?v=wygy721nzRc ")
+    }
+    else if (x.value=="Clementi") {
+        window.open("https://www.youtube.com/watch?v=3qq-1gCiB7g")
+    }
+    else if (x.value=="Debussy") {
+        window.open("https://www.youtube.com/watch?v=OUx6ZY60uiI")
+    }
+    else if (x.value=="Godowsky") {
+        window.open("https://www.youtube.com/watch?v=f0nlJXooIVc")
+    }
+    else if (x.value=="Granados") {
+        window.open("https://www.youtube.com/watch?v=gmu8OfcF8-Q")
+    }
+    else if (x.value=="Grieg") {
+        window.open("https://www.youtube.com/watch?v=R_5AwR27wj4")
+    }
+    else if (x.value=="Haydn") {
+        window.open("https://www.youtube.com/watch?v=EmZF3kBZQ6E")
+    }
+    else if (x.value=="Liszt") {
+        window.open("https://www.youtube.com/watch?v=salrwSVWpC4")
+    }
+    else if (x.value=="Mendelssohn") {
+        window.open("https://www.youtube.com/watch?v=sWiCHa9DFO")
+    }
+    else if (x.value=="Moszkowski") {
+        window.open("https://www.youtube.com/watch?v=EAE0VZRHNlo")
+    }
+    else if (x.value=="Mozart") {
+        window.open("https://www.youtube.com/watch?v=Rb0UmrCXxVA")
+    }
+    else if (x.value=="Mussorgsky") {
+        window.open("https://www.youtube.com/watch?v=_RyLq5Pbw8M")
+    }
+    else if (x.value=="Rachaminov") {
+        window.open("https://www.youtube.com/watch?v=vpaPWuDQUcc")
+    }
+    else if (x.value=="Ravel") {
+        window.open("https://www.youtube.com/watch?v=1_WmavzeFh4")
+    }
+    else if (x.value=="Schubert") {
+        window.open("https://www.youtube.com/watch?v=iiChxN0T2kA")
+    }
+    else if (x.value=="Schumann") {
+        window.open("https://www.youtube.com/watch?v=QqEWbOzL_GQ")
+    }
+    else if (x.value=="Sinding") {
+        window.open("https://www.youtube.com/watch?v=gWlwlGsQAek")
+    }
+    else if (x.value=="Tchaikovsky") {
+        window.open("https://www.youtube.com/watch?v=7_WWz2DSnT8")
+    }
+}
