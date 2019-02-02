98bf46102d1e23476d740a5824863d41ae8296b6
vctoriawu <wuvctoria16@gmail.com>
2019-02-02 09:28:56 -0500

    Updated file to get all twitter followers from a twitter handle

--- GetFollowers.py
+++ GetFollowers.py
@@ -1,6 +1,7 @@
 import tweepy
 import time
-#insert your Twitter keys here
+
+#insert Twitter keys here
 consumer_key ='LBmiVSC1no4EnykqxKfFZJ4em'
 consumer_secret='J3klllRhlzz7NmkkMGOdFt2xpMCwSzv1D6WVWsFbqcXP0DmKjc'
 access_token='334992084-6DctgZkCdNjvdcMjBzFBUxdPW2iFbKxq6alGHvNt'
@@ -11,34 +12,25 @@ auth = tweepy.auth.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+#Open a text file to write all the followers in
 list= open('twitter_followers.txt','w')
 
 if(api.verify_credentials):
     print ('We successfully logged in')
 
 user = tweepy.Cursor(api.followers, screen_name=twitter_handle).items()
-#for user in tweepy.Cursor(api.followers, screen_name=twitter_handle).items():
+
+#Iterate through all the followers
 while True:
 	try:
 		u = next(user)
 		list.write(u.screen_name +' \n')
 		print (u.screen_name)
+	#Reached limit for tweepy
 	except tweepy.RateLimitError:
-		#logging.info("[%s] Timeout Reached, Sleep for 15 minutes before restart" % self.process_name)
 		print ('We got a timeout ... Sleeping for 15 minutes')
 		time.sleep(15*60)
 	except StopIteration:
 		break
 
-# while True:
-#     try:
-#         u = next(user)
-#         list.write(u.screen_name +' \n')
 
-#     except:
-#         time.sleep(15*60)
-#         print ('We got a timeout ... Sleeping for 15 minutes')
-#         u = next(user)
-#         list.write(u.screen_name +' \n')
-
-# list.close()
