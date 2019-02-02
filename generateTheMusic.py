81c0a1f3a72af7b46c1bfd2426a11572f3daed1a
John Anthony <johndavid.anthony1@gmail.com>
2019-02-02 17:19:20 -0500

    Update generateTheMusic.py

--- generateTheMusic.py
+++ generateTheMusic.py
@@ -1,3 +1,19 @@
+import keras
+
+import glob
+import pickle
+import numpy
+from music21 import converter, instrument, note, chord, stream
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Activation
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+
+from music21 import converter, note, chord, instrument
+
 model = Sequential()
 model.add(LSTM(
     256,
